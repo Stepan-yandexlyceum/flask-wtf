9abffa524b0f07d4e77a1c6aa8aaf3669ce94f14
Stepan-yandexlyceum <gretoton@mail.ru>
2021-02-26 17:38:45 +0300

    я сделяль еще лучше

--- server.py
+++ server.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
 import json
+
 app = Flask(__name__)
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = "Ученик Яндекс.Лицея"
-    return render_template('index.html', title='заготовка')
+    param = {}
+    param['title'] = 'Заготовка'
+    return render_template('base.html', **param)
+
+if __name__ == '__main__':
+    app.run(port=8000, host='127.0.0.1')
