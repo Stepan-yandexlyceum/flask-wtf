4832d9f0d5bceeb1b5c2d820e61d07688a3e29cc
Stepan-yandexlyceum <gretoton@mail.ru>
2021-02-26 17:32:51 +0300

    я сделяль

---
+++ server.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+import json
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = "Ученик Яндекс.Лицея"
+    return render_template('index.html', title='заготовка')
