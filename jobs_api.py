258f2e65cf4460f193d11bf6fabe9d9ee15fdfce
Stepan-yandexluceum <gretoton@mail.ru>
2021-03-09 18:03:20 +0300

    ex 2 (job id and error handler)

--- jobs_api.py
+++ jobs_api.py
@@ -1,6 +1,7 @@
 import flask
 from flask import jsonify
 from jobs import Jobs
+from flask import make_response
 
 from . import db_session
 
@@ -20,6 +21,25 @@ def get_jobs():
             'jobs':
                 [item.to_dict(
                     only=('team_leader', 'job', 'work_size', 'collaborators', 'start_date', 'end_date', 'is_finished'))
-                 for item in jobs]
+                    for item in jobs]
         }
     )
+
+
+@blueprint.route('/api/jobs/<int:job_id>')
+def get_jobs(job_id):
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs).all()
+    item = jobs[job_id]
+    return jsonify(
+        {
+            'jobs':
+                [item.to_dict(
+                    only=('team_leader', 'job', 'work_size', 'collaborators', 'start_date', 'end_date', 'is_finished'))]
+        }
+    )
+
+
+@blueprint.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
