a35e14bbb0dff4d6b4d520f3af9e377f7c0a061e
Stepan-yandexlyceum <gretoton@mail.ru>
2021-03-16 18:09:06 +0300

    necessary files added

--- main.py
+++ main.py
@@ -361,7 +361,6 @@ if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
 
-
 def main():
     db_session.global_init("db/blogs.db")
     app.register_blueprint(jobs_api.blueprint)
